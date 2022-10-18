b6b1da3ed37411dcc216deff524e3ac3496684a7
Mekonnen T Tesfazien <mekonnen.tesfamichael@gmail.com>
2022-10-18 13:30:22 -0700

    Update App.js
    updating switch to routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 //import "./App.css";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Signin from "./Signin";
 import Profile from "./Profile";
 
@@ -14,14 +14,14 @@ function App() {
   return (
     <div className="wrapper">
       <BrowserRouter>
-        <Switch>
+        <Routes>
           <Route path="/profile">
             <Profile />
           </Route>
           <Route path="/">
             <Profile />
           </Route>
-        </Switch>
+        </Routes>
       </BrowserRouter>
     </div>
   );
