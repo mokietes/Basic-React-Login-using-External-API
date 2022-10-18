b64c10eb67a9517e0556942f158e793a998cb147
Mekonnen T Tesfazien <mekonnen.tesfamichael@gmail.com>
2022-10-17 23:17:02 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,21 +6,17 @@ import Profile from "./Profile";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/">
+            <Profile />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
